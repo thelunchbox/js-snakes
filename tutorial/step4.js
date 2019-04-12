f43926b4d4e7c506b05d7e5d3e337187d72a39be
Molly Henderson <molly.henderson@crosschx.com>
2019-04-12 17:55:13 -0400

    Fix whitespace!

--- tutorial/step4.js
+++ tutorial/step4.js
@@ -11,28 +11,28 @@ class SpiralOut extends Snake {
 
     update(dt, opponent, dimensions) {
         if (this.body.length == 1) {
-        const upPosition = this.moveUp();
-        if (this.isValidMove(upPosition, opponent, dimensions)) {
-            return upPosition;
-        }
-        else {
-            return this.moveDown();
-        }
+            const upPosition = this.moveUp();
+            if (this.isValidMove(upPosition, opponent, dimensions)) {
+                return upPosition;
+            }
+            else {
+                return this.moveDown();
+            }
         }
 
         const rightDirection = this.turnRight();
         if (this.isValidMove(rightDirection, opponent, dimensions)) {
-        return rightDirection;
+            return rightDirection;
         }
 
         const forwardDirection = this.goStraight();
         if (this.isValidMove(forwardDirection, opponent, dimensions)) {
-        return forwardDirection;
+            return forwardDirection;
         }
 
         const leftDirection = this.turnLeft();
         if (this.isValidMove(leftDirection, opponent, dimensions)) {
-        return leftDirection;
+            return leftDirection;
         }
 
         return this.moveDown();
