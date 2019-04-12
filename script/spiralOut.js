d0d99302b788b1e99032e4498e65b6ce458b03a9
Molly Henderson <molly.henderson@crosschx.com>
2019-04-12 17:34:59 -0400

    SpiralOut snake

---
+++ script/spiralOut.js
@@ -0,0 +1,48 @@
+ /*
+  * STRATEGY
+  * If this is the first move, go up or down.
+  * Otherwise, try turning.
+  * If the move isn't valid, try the next direction (right, forward, then left)
+  * If no moves work, give up
+ */
+class spiralOut extends Snake {
+
+    constructor() {
+      super({
+        color: '#372375',
+        accent: '#B28CFC',
+        name: 'Spiral Out',
+        author: 'HERO Hackathon',
+      });
+    }
+
+  update(dt, opponent, dimensions) {
+
+    if (this.body.length == 1) {
+      const upPosition = this.moveUp();
+      if (this.isValidMove(upPosition, opponent, dimensions)) {
+        return upPosition;
+      }
+      else {
+        return this.moveDown();
+      }
+    }
+
+    const rightDirection = this.turnRight();
+    if (this.isValidMove(rightDirection, opponent, dimensions)) {
+      return rightDirection;
+    }
+
+    const forwardDirection = this.goStraight();
+    if (this.isValidMove(forwardDirection, opponent, dimensions)) {
+      return forwardDirection;
+    }
+
+    const leftDirection = this.turnLeft();
+    if (this.isValidMove(leftDirection, opponent, dimensions)) {
+      return leftDirection;
+    }
+
+    return this.moveDown();
+  }
+}
